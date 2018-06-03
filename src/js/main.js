4c14d2b5945866f148b92d3a19d87053526048a0
Dimitrios Stefos <dim_stefos@outlook.com>
2018-06-03 14:01:31 +0930

    Addition of Icon boxes and Testimonials slider

--- src/js/main.js
+++ src/js/main.js
@@ -14,4 +14,10 @@ $(function() {
 $(document).on('click', '[data-toggle="lightbox"]', function(event) {
                 event.preventDefault();
                 $(this).ekkoLightbox();
-            });
+});
+
+$('.slider').slick({
+    infinite: true,
+    slideToShow: 1,
+    slideToScroll: 1
+  });
